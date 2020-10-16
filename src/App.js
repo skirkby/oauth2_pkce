df9d38e0265bb248b0e40642ee28f61c51cea953
Sean Kirkby <skrkby@kirkby.org>
2020-10-16 07:59:13 -0700

    removed login hint and hd from oauth url

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import './App.css';
 const host = 'https://accounts.google.com';
 const path = '/o/oauth2/v2/auth';
 const resptype = 'response_type=code';
-const clientid = 'client_id=424911365001.apps.googleusercontent.com';
+const clientid = '110998841151-0h8i2a5f23pndusr31i9sg83hntaeffg.apps.googleusercontent.com';
 const scopes = ['openid', 'email', 'profile'];
-const redirect = 'redirect_uri=https%3A//oauth2.example.com/code';
+const redirect = 'redirect_uri=https%3A//oauth-webapi.herokuapp.com/register';
 const state = 'state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2-login-demo.example.com%2FmyHome';
-const login_hint = 'login_hint=jsmith@example.com';
-const hd = 'hd=example.com';
+// const login_hint = 'login_hint=jsmith@example.com';
+// const hd = 'hd=example.com';
 
-let loginWithGoogleUrl = `${host}${path}?${resptype}&${clientid}&${redirect}&${state}&${login_hint}&${hd}&scope=`;
+let loginWithGoogleUrl = `${host}${path}?${resptype}&${clientid}&${redirect}&${state}&scope=`;
 scopes.forEach(scope => {
   loginWithGoogleUrl += `${scope}%20`;
 });
