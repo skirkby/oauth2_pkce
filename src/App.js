a1be8f44aa53ce03148bd32889c27b5fb0bffec1
Sean Kirkby <skrkby@kirkby.org>
2020-10-16 08:04:11 -0700

    fix client_id param

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import './App.css';
 const host = 'https://accounts.google.com';
 const path = '/o/oauth2/v2/auth';
 const resptype = 'response_type=code';
-const clientid = '110998841151-0h8i2a5f23pndusr31i9sg83hntaeffg.apps.googleusercontent.com';
+const clientid = 'client_id=110998841151-0h8i2a5f23pndusr31i9sg83hntaeffg.apps.googleusercontent.com';
 const scopes = ['openid', 'email', 'profile'];
 const redirect = 'redirect_uri=https%3A//oauth-webapi.herokuapp.com/register';
 const state = 'state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2-login-demo.example.com%2FmyHome';
-// const login_hint = 'login_hint=jsmith@example.com';
-// const hd = 'hd=example.com';
+const login_hint = 'login_hint=jsmith@example.com';
+const hd = 'hd=example.com';
 
 let loginWithGoogleUrl = `${host}${path}?${resptype}&${clientid}&${redirect}&${state}&scope=`;
 scopes.forEach(scope => {
