21b121249161f71d4862ebe9e0d3a955791c362d
Sean Kirkby <skrkby@kirkby.org>
2020-10-16 08:16:35 -0700

    modify redirect url

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const path = '/o/oauth2/v2/auth';
 const resptype = 'response_type=code';
 const clientid = 'client_id=110998841151-0h8i2a5f23pndusr31i9sg83hntaeffg.apps.googleusercontent.com';
 const scopes = ['openid', 'email', 'profile'];
-const redirect = 'redirect_uri=https%3A//oauth-webapi.herokuapp.com/register';
+const redirect = 'redirect_uri=https%3A//oauth-webapi.herokuapp.com/auth/register';
 const state = 'state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2-login-demo.example.com%2FmyHome';
 const login_hint = 'login_hint=jsmith@example.com';
 const hd = 'hd=example.com';
