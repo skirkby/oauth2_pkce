dd55562edce31f72fe0f6aae04e6e95afd2645c0
Sean Kirkby <skrkby@kirkby.org>
2020-10-20 14:01:54 -0600

    add google integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { OAuth2Client } from 'google-auth-library';
+import keys from './oauth2.keys.json';
 
 const host = 'https://accounts.google.com';
 const path = '/o/oauth2/v2/auth';
@@ -9,8 +11,8 @@ const clientid = 'client_id=110998841151-0h8i2a5f23pndusr31i9sg83hntaeffg.apps.g
 const scopes = ['openid', 'email', 'profile'];
 const redirect = 'redirect_uri=https%3A//oauth-webapi.herokuapp.com/auth/register';
 const state = 'state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2-login-demo.example.com%2FmyHome';
-const login_hint = 'login_hint=jsmith@example.com';
-const hd = 'hd=example.com';
+// const login_hint = 'login_hint=jsmith@example.com';
+// const hd = 'hd=example.com';
 
 let loginWithGoogleUrl = `${host}${path}?${resptype}&${clientid}&${redirect}&${state}&scope=`;
 scopes.forEach(scope => {
@@ -29,8 +31,23 @@ function App() {
 }
 
 function openGoogleUrl() {
-  console.log(loginWithGoogleUrl);
-  window.open(loginWithGoogleUrl, "_blank");
+
+  console.log(OAuth2Client)
+  const oAuth2Client = new OAuth2Client(
+    keys.web.client_id,
+    keys.web.client_secret,
+    keys.web.redirect_uris[0]
+  );
+
+  // Generate the url that will be used for the consent dialog.
+  const authorizeUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: 'https://www.googleapis.com/auth/userinfo.profile',
+  });
+
+
+  console.log(authorizeUrl);
+  window.open(authorizeUrl, "_blank");
 }
 
 export default App;
