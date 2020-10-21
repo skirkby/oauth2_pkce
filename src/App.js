cc74f7ce59c18ef204562c3700a24405622b77ea
Sean Kirkby <skirkby@kirkby.org>
2020-10-20 20:04:27 -0600

    set up pkce

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { OAuth2Client } from 'google-auth-library';
-import keys from './oauth2.keys.json';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useLocation
+} from "react-router-dom";
+import { OAuth2Client } from "google-auth-library";
+// import keys from './oauth2.keys.json';
+import keys from './pkce.keys.json';
 
-const host = 'https://accounts.google.com';
-const path = '/o/oauth2/v2/auth';
-const resptype = 'response_type=code';
-const clientid = 'client_id=110998841151-0h8i2a5f23pndusr31i9sg83hntaeffg.apps.googleusercontent.com';
-const scopes = ['openid', 'email', 'profile'];
-const redirect = 'redirect_uri=https%3A//oauth-webapi.herokuapp.com/auth/register';
-const state = 'state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2-login-demo.example.com%2FmyHome';
+// const host = 'https://accounts.google.com';
+// const path = '/o/oauth2/v2/auth';
+// const resptype = 'response_type=code';
+// const clientid = 'client_id=110998841151-0h8i2a5f23pndusr31i9sg83hntaeffg.apps.googleusercontent.com';
+// const scopes = ['openid', 'email', 'profile'];
+// const redirect = 'redirect_uri=https%3A//oauth-webapi.herokuapp.com/auth/register';
+// const state = 'state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2-login-demo.example.com%2FmyHome';
 // const login_hint = 'login_hint=jsmith@example.com';
 // const hd = 'hd=example.com';
 
-let loginWithGoogleUrl = `${host}${path}?${resptype}&${clientid}&${redirect}&${state}&scope=`;
-scopes.forEach(scope => {
-  loginWithGoogleUrl += `${scope}%20`;
-});
+// let loginWithGoogleUrl = `${host}${path}?${resptype}&${clientid}&${redirect}&${state}&scope=`;
+// scopes.forEach(scope => {
+//   loginWithGoogleUrl += `${scope}%20`;
+// });
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Login:</p>
-        <button onClick={openGoogleUrl}>Login with Google</button>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>Login:</p>
+          <button onClick={openGoogleUrl}>Login with Google</button>
+        </header>
+      </div>
+
+      <Switch>
+        <Route path="/oauthcallback">
+          <CodePanel />
+        </Route>
+      </Switch>
+    </Router>
   );
+}
+
+function CodePanel() {
+  let query = useQuery();
+  let code = query.get("code");
+  return (<div>code = {code} </div>);
 }
 
 function openGoogleUrl() {
@@ -48,6 +72,10 @@ function openGoogleUrl() {
 
   console.log(authorizeUrl);
   window.open(authorizeUrl, "_blank");
+}
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
 }
 
 export default App;
